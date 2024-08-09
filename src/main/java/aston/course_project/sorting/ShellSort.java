4101e588865d8bfc5c741a68dae5b355fb225262
redmopag <redmopag@gmail.com>
2024-08-09 19:29:28 +0500

    Реализовать алгоритм сортировки Шелла в классе ShellSort

---
+++ src/main/java/aston/course_project/sorting/ShellSort.java
@@ -0,0 +1,29 @@
+package aston.course_project.sorting;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class ShellSort<T> {
+    Comparator<T> comparator;
+    public ShellSort(Comparator<T> comparator){
+        this.comparator = comparator;
+    }
+
+    public void shellSort(List<T> toSort){
+        int n = toSort.size(); // Кол-во элементов
+
+        // h - прыжок для выбора значения в подмножество
+        for(int h = n / 2; h > 0; h /= 2){
+            for(int i = h; i < n; i++){
+                T key = toSort.get(i);
+                int j = i;
+                // Проверяем, если элемент key меньше элемента из своего подмножества, то сортируем вставкой
+                while(j >= h && comparator.compare(key, toSort.get(j - h)) < 0){
+                    toSort.set(j, toSort.get(j - h));
+                    j -= h;
+                }
+                toSort.set(j, key);
+            }
+        }
+    }
+}
