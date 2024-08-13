51932c459f440f3ea454f68842a4061183ef82a9
Dmitrii <akmeev2011@yandex.ru>
2024-08-13 14:17:56 +0700

    1. изменить метод бинарного поиска 2.изменить тесты

--- src/test/java/BinarySearchTest.java
+++ src/test/java/BinarySearchTest.java
@@ -13,11 +13,9 @@ public class BinarySearchTest {
         //отсортированный список
         List<Integer> list = List.of(1, 2, 3);
         BinarySearch<Integer> bin = new BinarySearch<>();
-        //пока что с компаратором
-        Comparator<Integer> comparator = (Comparator.comparingInt(t -> t));
         // возвращает -1, если нужного элемента нет
-        Assertions.assertEquals(bin.binarySearch(list,7, comparator), -1);
+        Assertions.assertEquals(bin.binarySearch(list,7), -1);
         //возвращает индекс элемента из списка, который мы ищем
-        Assertions.assertEquals(bin.binarySearch(list,1, comparator), 0);
+        Assertions.assertEquals(bin.binarySearch(list,1), 0);
     }
 }
