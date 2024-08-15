4418876f13c5172430bd50aefceb337819f5827a
redmopag <redmopag@gmail.com>
2024-08-15 17:11:39 +0500

    Добавить исключение для неверного значения поля класса

---
+++ src/main/java/aston/course_project/sorting/exceptions/InvalidAgrumentException.java
@@ -0,0 +1,7 @@
+package aston.course_project.sorting.exceptions;
+
+public class InvalidAgrumentException extends Exception{
+    public InvalidAgrumentException(String message){
+        super(message);
+    }
+}
