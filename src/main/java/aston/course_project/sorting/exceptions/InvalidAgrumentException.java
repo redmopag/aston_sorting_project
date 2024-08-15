1a9dcde4974175b9e9faf35452b85bc914574eb8
redmopag <redmopag@gmail.com>
2024-08-15 18:01:06 +0500

    Удалить продублированный класс InvalidAgrumentException

--- src/main/java/aston/course_project/sorting/exceptions/InvalidAgrumentException.java
+++
@@ -1,7 +0,0 @@
-package aston.course_project.sorting.exceptions;
-
-public class InvalidAgrumentException extends Exception{
-    public InvalidAgrumentException(String message){
-        super(message);
-    }
-}
