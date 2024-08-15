58f9da9754dac77a3d019e4765f6f3abe1aa5835
nick-pronin <vertupro061@gmail.com>
2024-08-15 22:28:06 +0300

    Без рефлексии

--- src/main/java/aston/course_project/sorting/FillArraysOptions/FillArray.java
+++ src/main/java/aston/course_project/sorting/FillArraysOptions/FillArray.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class FillArrayWithoutReflection implements ArrayFillOption {
+public class FillArray implements ArrayFillOption {
     private final Scanner scanner = new Scanner(System.in);
 
     @Override
